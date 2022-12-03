ddbfe2384579ec88b3d419f8fb9b9102506c3474
Robert Leung <rleung84@uwo.ca>
2022-12-03 17:18:19 -0500

    Added tab switching logic

--- src/index.js
+++ src/index.js
@@ -3,14 +3,14 @@ import menuPage from "./pages/menu";
 import contactPage from "./pages/contact";
 import './style.css';
 
-function navbar() {
+function loadPage() {
   const content = document.querySelector('#content')
   const nav = document.createElement('div')
   nav.classList.add('nav')
   const tabs = document.createElement('div')
   tabs.classList.add('tabs')
   const homeTab = document.createElement('button')
-  homeTab.classList.add('tab')
+  homeTab.classList.add('tab', 'tab-active')
   homeTab.innerText = 'Home'
   homeTab.addEventListener('click', mainContent)
   const menuTab = document.createElement('button')
@@ -26,13 +26,28 @@ function navbar() {
   tabs.appendChild(contactTab)
   nav.appendChild(tabs)
   content.appendChild(nav)
+  homePage()
 }
 
 function mainContent(e) {
-  console.log(e.currentTarget.innerText)
+  e.currentTarget.classList.add("tab-active")
+  const main = document.querySelector('.main')
+  main.remove()
+  const tab = document.getElementsByClassName("tab");
+  for (let i = 0; i < tab.length; i++) {
+    tab[i].className = tab[i].className.replace("tab-active", "");
+  }
+  if (e.currentTarget.innerText == "Home") {
+    homePage()
+  } else if (e.currentTarget.innerText == "Menu") {
+    menuPage()
+  }
+  else {
+    contactPage()
+  }
 }
 
-navbar()
+loadPage()
 // Depending on which tab is selected render the page
 // homePage()
 // menuPage()
